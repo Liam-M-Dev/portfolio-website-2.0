00eb2c5a9e1948e90a1318c1c6596f00ca92c907
Liam-M-Dev <liammassey21@gmail.com>
2023-05-29 17:44:24 +1000

    Add images to assest folder, begin scaffolding homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { NavBar } from "./components/NavBar"
-import { HomePage } from "./pages/HomePage"
+import  HomePage  from "./pages/HomePage"
 import { AboutPage } from "./pages/About"
 import { ProjectPage } from "./pages/Projects"
 import { ContactPage } from "./pages/Contact"
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,20 @@
 
 
 
-export function HomePage(){
+export default function HomePage(){
 
     return(
         <div>
-            <h1>Home</h1>
+            <main>
+            <h1>
+                Liam Massey
+            </h1>
+
+            <figure>
+                <img src="src/assets/liamlogo.png" alt="Logo for Liam Massey web development, acts as homepage button" />
+            </figure>
+        </main>
         </div>
+        
     )
 }
