107452a21d0b5079381fbde9f4520c02cb29e7fb
Liam-M-Dev <liammassey21@gmail.com>
2023-06-05 21:05:24 +1000

    Fixing relative path link

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ export default function HomePage(){
             </h1>
 
             <figure className="overflow-hidden max-w-tenPercentLess">
-                <img className="mt-5 mr-40" src="../assets/liamlogo.png" alt="Logo for Liam Massey web development, acts as homepage button" />
+                <img className="mt-5 mr-40" src="../../src/assets/liamlogo.png" alt="Logo for Liam Massey web development, acts as homepage button" />
             </figure>
 
             <h3 className="font-heading mt-10 mb-5 text-center text-2xl">Full Stack Developer</h3>
