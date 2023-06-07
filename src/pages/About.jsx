8ee24915edc4f2742772922820c1a5c52b849ad5
Liam-M-Dev <liammassey21@gmail.com>
2023-06-07 14:57:47 +1000

    Change font sizes to see impact on phones

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,11 +6,11 @@ export function AboutPage(){
     return(
         <main className="box-border flex flex-col justify-center">
             <section>
-                <h1 className="font-heading text-lg font-bold my-2 ml-5">
+                <h1 className="font-heading text-base font-bold my-2 ml-5">
                     About Me
                 </h1>
 
-                <p className="font-content text-base mx-3">
+                <p className="font-content text-sm mx-3">
                     My name is Liam and I&apos;m a <strong>junior full stack web developer</strong>.
                     I&apos;m a creative person with a natural desire to problem solve and take on challenges.
                     What interests me with web development, is the process of translating an idea into a fully functional site.
@@ -19,7 +19,7 @@ export function AboutPage(){
                     If you would like to get in contact with me about potential collaborations or any queries please contact me via the contact page.
                 </p>
                 
-                <figure className="flex flex-row mt-10">
+                <figure className="flex flex-row mt-5">
                     <img className="object-contain w-52 " src={myself} alt="A photo of my cat Patti and myself" />
                     <img className="object-contain w-52 " src={playing} alt="A photo of myself playing a board game" />
                 </figure>
